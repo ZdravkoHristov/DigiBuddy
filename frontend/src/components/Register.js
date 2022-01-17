8bc437f48cf92cb916abd3cc2d3fe5351dab22cc
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-01-17 14:13:26 +0200

    added register

---
+++ frontend/src/components/Register.js
@@ -0,0 +1,4 @@
+import RegisterEl from './styles/Register.style.js';
+export default function Register() {
+	return <RegisterEl id='register'></RegisterEl>;
+}
---
+++ frontend/src/components/styles/Register.style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const RegisterEl = styled.section``;
+
+export default RegisterEl;
