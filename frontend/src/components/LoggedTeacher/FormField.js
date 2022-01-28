97df63c19e39feaf1ade90e8a50e3cdafd3ba129
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-01-28 12:36:56 +0200

    returned the icons to the settings

--- frontend/src/components/LoggedTeacher/FormField.js
+++ frontend/src/components/LoggedTeacher/FormField.js
@@ -2,10 +2,15 @@ import { useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { teacherSelector } from '../../store/store';
 export default function FormField({ labelTxt, dataChunk, inputType = 'text' }) {
+	const [editing, setEditing] = useState(false);
 	const data = useSelector(teacherSelector).info[dataChunk];
-
 	const [value, setValue] = useState(data);
 	const inputRef = useRef();
+
+	const handleIconClick = () => {
+		inputRef.current.focus();
+		setEditing(!editing);
+	};
 
 	return (
 		<div className='form-field'>
@@ -17,7 +22,11 @@ export default function FormField({ labelTxt, dataChunk, inputType = 'text' }) {
 				onChange={e => setValue(e.target.value)}
 				size={data.length * 1.2}
 				ref={inputRef}
+				readOnly={!editing}
 			/>
+			<span className='icon' onClick={handleIconClick}>
+				{editing ? null : <i className='fas fa-edit'></i>}
+			</span>
 		</div>
 	);
 }
