726f013a6fd6dc21e2719f98f21bd4bc0d4c578a
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-02-25 12:13:20 +0200

    Changed the min-height of the header and added a navbar item styles breakpoint for 1800px

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,78 +1,82 @@
-import { useSelector } from 'react-redux';
-import { homeStateSelector } from '../store/store';
-import Header from './Header';
-import AnimatedLine from './AnimatedLine';
-import AnimatedElement from './AnimatedElement';
-import RolesSection from './RolesSection';
-import RegTeacher from './RegTeacher';
-import RegStudent from './RegStudent';
-import LogTeacher from './LogTeacher';
-import LogStudent from './LogStudent';
-import Faq from './Faq';
-import Navbar from './Navbar';
-import headerBackground from '../assets/svgs/white-space-header.svg';
-import heroIllustration from '../assets/illustrations/home-image.svg';
+import { useSelector } from "react-redux";
+import { homeStateSelector } from "../store/store";
+import Header from "./Header";
+import AnimatedLine from "./AnimatedLine";
+import AnimatedElement from "./AnimatedElement";
+import RolesSection from "./RolesSection";
+import RegTeacher from "./RegTeacher";
+import RegStudent from "./RegStudent";
+import LogTeacher from "./LogTeacher";
+import LogStudent from "./LogStudent";
+import Faq from "./Faq";
+import Navbar from "./Navbar";
+import headerBackground from "../assets/svgs/white-space-header.svg";
+import heroIllustration from "../assets/illustrations/home-image.svg";
 
-import '../Home.css';
+import "../Home.css";
 
 export default function Home() {
-	const { activeForm, showForm } = useSelector(homeStateSelector).data;
+    const { activeForm, showForm } = useSelector(homeStateSelector).data;
 
-	const navLinks = [
-		{ text: 'Начало', to: '#', value: 'home' },
-		{ text: 'Вход', to: '#entrance', value: 'entrance' },
-		{ text: 'ЧЗВ', to: '#faq', value: 'faq' },
-	];
+    const navLinks = [
+        { text: "Начало", to: "#", value: "home" },
+        { text: "Вход", to: "#entrance", value: "entrance" },
+        { text: "ЧЗВ", to: "#faq", value: "faq" },
+    ];
 
-	const navBreakpoints = new Map();
-	navBreakpoints.set(800, 3);
+    const navBreakpoints = new Map();
+    navBreakpoints.set(1800, 3);
 
-	const HeroContent = props => {
-		return (
-			<section {...props}>
-				{' '}
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
-					quibusdam qui adipisci, culpa odio veniam nobis! Corporis vel
-					reprehenderit eos.
-				</p>
-				<div className='img-holder'>
-					<img src={heroIllustration} alt='illustration' />
-				</div>
-			</section>
-		);
-	};
+    const HeroContent = (props) => {
+        return (
+            <section {...props}>
+                {" "}
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Repellat quibusdam qui adipisci, culpa odio veniam nobis!
+                    Corporis vel reprehenderit eos.
+                </p>
+                <div className="img-holder">
+                    <img src={heroIllustration} alt="illustration" />
+                </div>
+            </section>
+        );
+    };
 
-	return (
-		<>
-			<div className='gradient-holder'>
-				<Header
-					heroContent={<HeroContent />}
-					style={{
-						backgroundImage: `url("${headerBackground}")`,
-						backgroundPositionY: 'bottom',
-					}}
-				>
-					<Navbar links={navLinks} outCount={2} breakpoints={navBreakpoints} />
-					<HeroContent className='hero container' />
-				</Header>
-				<AnimatedLine />
-				<RolesSection />
-				<AnimatedElement
-					isMounted={showForm}
-					inClass={'slide-down-animation'}
-					outClass='slide-up-animation'
-					delayTime={700}
-				>
-					{console.log(showForm, activeForm)}
-					{activeForm === 'regTeacher' && <RegTeacher />}
-					{activeForm === 'regStudent' && <RegStudent />}
-					{activeForm === 'logTeacher' && <LogTeacher />}
-					{activeForm === 'logStudent' && <LogStudent />}
-				</AnimatedElement>
+    return (
+        <>
+            <div className="gradient-holder">
+                <Header
+                    heroContent={<HeroContent />}
+                    style={{
+                        backgroundImage: `url("${headerBackground}")`,
+                        backgroundPositionY: "bottom",
+                    }}
+                >
+                    <Navbar
+                        links={navLinks}
+                        outCount={2}
+                        breakpoints={navBreakpoints}
+                    />
+                    <HeroContent className="hero container" />
+                </Header>
+                <AnimatedLine />
+                <RolesSection />
+                <AnimatedElement
+                    isMounted={showForm}
+                    inClass={"slide-down-animation"}
+                    outClass="slide-up-animation"
+                    delayTime={700}
+                >
+                    {console.log(showForm, activeForm)}
+                    {activeForm === "regTeacher" && <RegTeacher />}
+                    {activeForm === "regStudent" && <RegStudent />}
+                    {activeForm === "logTeacher" && <LogTeacher />}
+                    {activeForm === "logStudent" && <LogStudent />}
+                </AnimatedElement>
 
-				<Faq />
-			</div>
-		</>
-	);
+                <Faq />
+            </div>
+        </>
+    );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,98 +1,98 @@
-import { useEffect, useState, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { navbarSelector } from '../store/store';
-import { setActive } from '../store/slices/navbarSlice';
-import NavbarEl from './styles/Navbar.style';
-import logo from '../assets/logo/logo-short.svg';
+import { useEffect, useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { navbarSelector } from "../store/store";
+import { setActive } from "../store/slices/navbarSlice";
+import NavbarEl from "./styles/Navbar.style";
+import logo from "../assets/logo/logo-short.svg";
 
 export default function Navbar({ links, outCount, breakpoints }) {
-	const dispatch = useDispatch();
-	const { active } = useSelector(navbarSelector);
-	const [outItemCount, setOutItemCount] = useState(outCount);
-	const [isSticky, setIsSticky] = useState(false);
-	const menuRef = useRef();
-	const getActiveClass = checkFor => {
-		return active === checkFor ? ' active' : '';
-	};
+    const dispatch = useDispatch();
+    const { active } = useSelector(navbarSelector);
+    const [outItemCount, setOutItemCount] = useState(outCount);
+    const [isSticky, setIsSticky] = useState(false);
+    const menuRef = useRef();
+    const getActiveClass = (checkFor) => {
+        return active === checkFor ? " active" : "";
+    };
 
-	useEffect(() => {
-		const handleSticky = e => {
-			setIsSticky(window.scrollY > 50);
-		};
-		window.addEventListener('scroll', handleSticky);
+    useEffect(() => {
+        const handleSticky = (e) => {
+            setIsSticky(window.scrollY > 50);
+        };
+        window.addEventListener("scroll", handleSticky);
 
-		return () => {
-			window.removeEventListener('scroll', handleSticky);
-		};
-	}, []);
+        return () => {
+            window.removeEventListener("scroll", handleSticky);
+        };
+    }, []);
 
-	const checkBreakpoints = () => {
-		const currentWidth = window.innerWidth;
-		let newCount = outCount;
+    const checkBreakpoints = () => {
+        const currentWidth = window.innerWidth;
+        let newCount = outCount;
 
-		for (const pair of breakpoints) {
-			if (currentWidth < pair[0]) {
-				newCount = pair[1];
-				break;
-			}
-		}
+        for (const pair of breakpoints) {
+            if (currentWidth < pair[0]) {
+                newCount = pair[1];
+                break;
+            }
+        }
 
-		setOutItemCount(newCount);
-	};
+        setOutItemCount(newCount);
+    };
 
-	const handleClasses = () => {
-		const children = menuRef.current.childNodes;
-		for (let i = 0; i < children.length; i++) {
-			const item = children[i];
-			if (isSticky || i < outItemCount) {
-				item.classList.remove('in');
-				item.classList.add('out');
-			} else {
-				item.classList.remove('out');
-				item.classList.add('in');
-			}
-		}
-	};
+    const handleClasses = () => {
+        const children = menuRef.current.childNodes;
+        for (let i = 0; i < children.length; i++) {
+            const item = children[i];
+            if (isSticky || i < outItemCount) {
+                item.classList.remove("in");
+                item.classList.add("out");
+            } else {
+                item.classList.remove("out");
+                item.classList.add("in");
+            }
+        }
+    };
 
-	useEffect(handleClasses);
+    useEffect(handleClasses);
 
-	useEffect(() => {
-		if (!breakpoints) return;
-		checkBreakpoints();
+    useEffect(() => {
+        if (!breakpoints) return;
+        checkBreakpoints();
 
-		window.addEventListener('resize', checkBreakpoints);
+        window.addEventListener("resize", checkBreakpoints);
 
-		return () => window.removeEventListener('resize', checkBreakpoints);
-	}, [breakpoints]);
+        return () => window.removeEventListener("resize", checkBreakpoints);
+    }, [breakpoints]);
 
-	useEffect(() => {
-		dispatch(setActive('home'));
-	}, []);
+    useEffect(() => {
+        dispatch(setActive("home"));
+    }, []);
 
-	return (
-		<NavbarEl className={'main-nav ' + (isSticky ? 'sticky' : '')}>
-			{console.log(isSticky)}
-			<div className='container'>
-				<div className='logo-holder'>
-					<img src={logo} alt='logo' />
-				</div>
+    return (
+        <NavbarEl className={"main-nav " + (isSticky ? "sticky" : "")}>
+            {console.log(isSticky)}
+            <div className="container">
+                <div className="logo-holder">
+                    <img src={logo} alt="logo" />
+                </div>
 
-				<ul className='main-menu' ref={menuRef}>
-					{links.map(link => {
-						const activeClass = getActiveClass(link.value);
-						const className = 'menu-item' + activeClass;
-						return (
-							<li
-								className={className}
-								onClick={() => dispatch(setActive(link.value))}
-								key={link.text}
-							>
-								<a href={link.to}>{link.text}</a>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		</NavbarEl>
-	);
+                <ul className="main-menu" ref={menuRef}>
+                    {links.map((link) => {
+                        const activeClass = getActiveClass(link.value);
+                        const className = "menu-item" + activeClass;
+                        return (
+                            <li
+                                className={className}
+                                onClick={() => dispatch(setActive(link.value))}
+                                key={link.text}
+                            >
+                                <a href={link.to}>{link.text}</a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </NavbarEl>
+    );
 }
