735d2aea4174c392e46d936b4e020604a6bef16a
Zdravko Hristov <zdravkohristov0@gmail.com>
2022-03-03 14:15:53 +0200

    Moved the css from loggedTeacher.css to a styled-component file

--- frontend/src/components/LoggedTeacher/LoggedTeacher.js
+++ frontend/src/components/LoggedTeacher/LoggedTeacher.js
@@ -1,50 +1,56 @@
-import { useSelector } from 'react-redux';
-import { navbarSelector } from '../../store/store';
-import Header from '../Header';
-import Navbar from '../Navbar';
+import { useSelector } from "react-redux";
+import { navbarSelector } from "../../store/store";
+import Header from "../Header";
+import Navbar from "../Navbar";
 
-import Home from '../LoggedHome';
-import Worksheets from './Worksheets';
-import MyClasses from './MyClasses';
-import LoggedProfile from '../LoggedProfile';
-import MyProfile from './MyProfile';
-import Settings from './Settings';
-import Assignments from './Assignments';
-import headerBackground from '../../assets/svgs/full-back-teacher-back.svg';
-import '../../LoggedTeacher.css';
+import Home from "../LoggedHome";
+import Worksheets from "./Worksheets";
+import MyClasses from "./MyClasses";
+import LoggedProfile from "../LoggedProfile";
+import MyProfile from "./MyProfile";
+import Settings from "./Settings";
+import Assignments from "./Assignments";
+import headerBackground from "../../assets/svgs/full-back-teacher-back.svg";
+import TeacherEl from "../styles/LoggedTeacher.style";
 
 export default function LoggedTeacher() {
-	const { active: navActive } = useSelector(navbarSelector);
+    const { active: navActive } = useSelector(navbarSelector);
 
-	const navLinks = [
-		{ text: 'Начало', to: '#', value: 'home' },
+    const navLinks = [
+        { text: "Начало", to: "#", value: "home" },
 
-		{ text: 'Задачи', to: '#', value: 'assignments' },
-		{ text: 'Работни листове', to: '#', value: 'worksheets' },
-		{ text: 'Класове', to: '#', value: 'classes' },
-		{ text: 'Профил', to: '#', value: 'profile' },
-		{ text: 'Изход', to: '#', value: 'exit' },
-	];
+        { text: "Задачи", to: "#", value: "assignments" },
+        { text: "Работни листове", to: "#", value: "worksheets" },
+        { text: "Класове", to: "#", value: "classes" },
+        { text: "Профил", to: "#", value: "profile" },
+        { text: "Изход", to: "#", value: "exit" },
+    ];
 
-	return (
-		<>
-			<div className='gradient-holder'>
-				<Header style={{ backgroundImage: `url("${headerBackground}")` }}>
-					<Navbar links={navLinks} outCount={2}></Navbar>
-					<div style={{ padding: '4rem 1rem 1rem 1rem' }}>
-						{navActive === 'home' && <Home text='учител' />}
-						{navActive === 'worksheets' && <Worksheets />}
-						{navActive === 'classes' && <MyClasses />}
-						{navActive === 'assignments' && <Assignments />}
-						{navActive === 'profile' && (
-							<LoggedProfile
-								myProfile={<MyProfile />}
-								settings={<Settings />}
-							/>
-						)}
-					</div>
-				</Header>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <TeacherEl>
+                <div className="gradient-holder">
+                    <Header
+                        style={{
+                            backgroundImage: `url("${headerBackground}")`,
+                        }}
+                    >
+                        <Navbar links={navLinks} outCount={2}></Navbar>
+                        <div style={{ padding: "4rem 1rem 1rem 1rem" }}>
+                            {navActive === "home" && <Home text="учител" />}
+                            {navActive === "worksheets" && <Worksheets />}
+                            {navActive === "classes" && <MyClasses />}
+                            {navActive === "assignments" && <Assignments />}
+                            {navActive === "profile" && (
+                                <LoggedProfile
+                                    myProfile={<MyProfile />}
+                                    settings={<Settings />}
+                                />
+                            )}
+                        </div>
+                    </Header>
+                </div>
+            </TeacherEl>
+        </>
+    );
 }
