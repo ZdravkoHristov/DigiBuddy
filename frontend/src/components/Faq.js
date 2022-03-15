9b72029d2fcff6b05be6de62498e2199b97f8b97
ZdravkoHr <42611094+ZdravkoHr@users.noreply.github.com>
2022-03-15 11:26:38 +0200

    Added real data to FAQ

--- frontend/src/components/Faq.js
+++ frontend/src/components/Faq.js
@@ -8,21 +8,7 @@ export default function Faq(props) {
 				<h2 className='heading-l'>Често задавани въпроси: </h2>
 
 				<article className='questions'>
-					<FaqItem
-						number='1'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-					/>
-					<FaqItem
-						number='2'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-					/>
-					<FaqItem
-						number='3'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-					/>
+					<FaqItem						number='1'						question='Как се организират файловете?'						answer='Създадените от учителите задачи могат да се съкращават в папки под формата на файлове.'					/>					<FaqItem						number='2'						question='Как мога да се присъединя към клас?'						answer='Учителите изпращат код за присъединявяне в клас на учениците си.' />					<FaqItem						number='3'						question='Възможно ли е един и същ тест да бъде използван повече от веднъж?'						answer='Всички тестове се запазват в "Колекции" и могат да бъдат възлагани на различни ученици многократно.'					/>
 				</article>
 			</div>
 		</FaqEl>
