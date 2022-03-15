7fc49a40958c96002a56e8646d7a7b360851bab2
ZdravkoHr <42611094+ZdravkoHr@users.noreply.github.com>
2022-03-15 11:25:15 +0200

    Added real indo to FAQ

--- frontend/src/components/Faq.js
+++ frontend/src/components/Faq.js
@@ -10,18 +10,17 @@ export default function Faq() {
 				<article className='questions'>
 					<FaqItem
 						number='1'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
+						question='Как се организират файловете?'
+						answer='Създадените от учителите задачи могат да се съкращават в папки под формата на файлове.'
 					/>
 					<FaqItem
 						number='2'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-					/>
+						question='Как мога да се присъединя към клас?'
+						answer='Учителите изпращат код за присъединявяне в клас на учениците си.' />
 					<FaqItem
 						number='3'
-						question='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
-						answer='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus tempore voluptatum nihil. Quidem, consequatur voluptas?'
+						question='Възможно ли е един и същ тест да бъде използван повече от веднъж?'
+						answer='Всички тестове се запазват в "Колекции" и могат да бъдат възлагани на различни ученици многократно.'
 					/>
 				</article>
 			</div>
